6ac39d646a00002a9bc8db0a29beded85763057c
Collinsedumadze <egyiredumadzecollins@gmail.com>
2025-02-17 03:36:19 +0000

    working hours

--- hello.py
+++ hello.py
@@ -1,1 +1,12 @@
-print("helloworld")
+hour_worked = int(input('How many hours have u worked: '))
+
+rate = float(input('Rate per hour is : '))
+
+weekly_gross_pay = hour_worked * rate * 1
+
+monthly_gross_pay = hour_worked * rate * 4
+
+yearly_gross_pay =  hour_worked * rate * 52
+
+print(f"The grossly pay for the week is {weekly_gross_pay} "
+      f", month is {monthly_gross_pay} and year is {yearly_gross_pay}")
