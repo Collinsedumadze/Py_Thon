cf31018663262b629564fc6bd5a5c30876f02754
Collinsedumadze <egyiredumadzecollins@gmail.com>
2025-02-24 00:34:01 +0000

    some practice on tuple&list

--- student.py
+++ student.py
@@ -1,3 +1,4 @@
+"""
 def main():
     name = get_name()
     house = get_house()
@@ -9,7 +10,63 @@ def get_name():
 
 
 def get_house():
-    return input("House: ")    
+    return input("House: ")  
 
-if __name__ == "__main__":
+
+if __name__ =="__main__":
     main()
+
+
+#Using unpacking method
+
+def main():
+    name, house = student()
+    print(f"{name} lives in {house}")
+
+
+def student():
+    return input("Name: "), input("House: ") 
+ 
+
+
+if __name__ =="__main__":
+    main()
+
+      
+
+#Using Tuple and List method but the difference is their mutability
+
+def main():
+    student = Student()
+    print(type(student))
+    print(f"{student[0]} lives in {student[1]}")
+
+
+def Student():
+    return input("Name: "), input("House: ") 
+
+
+if __name__ =="__main__":
+    main()
+
+
+#Using Tuple and List method but the difference is their mutability
+
+def main():
+    student = Student()
+    print(type(student))
+    print( 
+    f"{student[0]} lives in {student[1]}")
+
+
+def Student():
+    return input("Name: "), input("House: ") 
+
+
+if __name__ =="__main__":
+    main()
+"""
+
+for i in range(0,10):
+    print(i )
+print('end')
