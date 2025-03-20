cb6f95732db2bf734eea7ca069eaa08298b6bb97
Collinsedumadze <egyiredumadzecollins@gmail.com>
2025-03-20 04:47:32 +0000

    tutorials

---
+++ CS50P/hello.py
@@ -0,0 +1,18 @@
+#Ask user for their name 
+name = input("What is your name? ")
+
+#Say hello to user
+print("hello,", end="")
+print(name)
+
+#Remove whitespace form string
+print("hello,", name.strip())
+ 
+#Capitalizing user's string
+print("hello,", name.capitalize())
+#Uppercase user's strings
+print("hello,", name.title())
+
+#Escape keyword
+print("hello, \"David\"")
+
